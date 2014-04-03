ad1328c1bde481a90cb28f92ee32efc99e8d94e9
JOSELV <>
2014-04-03 19:33:56 +0200

    03/04/2014 interface bolsa e inversor

---
+++ Ejercicios/ExerBolsa161/src/Bolsa161/Exercicio161ejecucion.java
@@ -0,0 +1,30 @@
+
+package Bolsa161;
+
+/**
+ *
+ * @author JRLV
+ */
+public class Exercicio161ejecucion {
+    
+    public static void main(String[] args) {
+        
+        BolsaEnBD novaB = new BolsaEnBD();
+        novaB.iniciar();
+        novaB.inserirAccions(1, "Zara", 100);
+        novaB.inserirAccions(2, "Mercadona", 50);
+        novaB.novo("Manolo", "qwerty", 4000);
+        novaB.novo("Pepe", "1234", 2000);
+        novaB.novo("Marisa", "qaz", 7000);
+        if(novaB.identificar("Pepe", "1234")){
+            Cliente novoC = new Cliente();
+            novoC.setLogin("Pepe");
+            //novoC.comprar(2, 2);
+            //novoC.vender(1, 1);
+            novoC.valorar();
+        } else{
+            System.out.println("Cliente seleccionado descoñecido.");
+        }
+        //novaB.actualizar();    
+    } 
+}
